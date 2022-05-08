4a66129ac6ae4e2ebf58057425e20f3cc4748611
yandori07 <90429330+yandori07@users.noreply.github.com>
2022-05-07 20:40:13 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,63 @@
+let ground;
+let lander;
+var lander_img;
+var bg_img;
+
+
+var vx = 0;
+var g = 0.05;
+var vy = 0;
+
+function preload()
+{
+  lander_img = loadImage("normal.png");
+  bg_img = loadImage("bg.png");
+
+}
+
+function setup() {
+  createCanvas(1000,700);
+  frameRate(80);
+
+  lander = createSprite(100,50,30,30);
+  lander.addImage(lander_img);
+  lander.scale = 0.1;
+
+  lander.setCollider("rectangle",0,0,200,200);
+
+  rectMode(CENTER);
+  textSize(15);
+}
+
+function draw() 
+{
+  background(51);
+ image(bg_img,0,0);
+ push();
+fill(255);
+text("Vertical Velocity :"+round(vy),800,75);
+pop();
+
+  vy+=g;
+  lander.position.y+=vy;
+
+  drawSprites();
+  
+
+}
+
+
+function keyPressed(){
+
+ if(keyDown(UP_ARROW)){
+
+  upward_thrust();
+  lander.changeAnimation('thrusting');
+   thrust.n
+ }}
+
+ function  upward_thrust(){
+  vy=-1;
+ 
+ }
+ 
